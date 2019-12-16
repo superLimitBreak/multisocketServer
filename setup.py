4b5379a44a8ab980357a7342d010ab9a30eccaae
Allan Callaghan <calaldees@gmail.com>
2019-12-16 18:14:56 +0000

    Add install_requires

--- setup.py
+++ setup.py
@@ -3,6 +3,20 @@ from setuptools import setup, find_packages
 
 _here = path.dirname(path.abspath(__file__))
 
+
+# https://stackoverflow.com/a/16624700/3356840
+#from pip.req import parse_requirements
+# `parse_requirements` is not in python2 - this is simple re-implementation
+import re
+REGEX_COMMENT = re.compile(r'(?:[\s^]#(.*))|(git\+.*)')
+def parse_requirements(filename):
+    with open(filename, 'rt') as filehandle:
+        return tuple(filter(None, (
+            REGEX_COMMENT.sub('', line).strip()
+            for line in filehandle
+        )))
+
+
 setup(
     name='multisocketServer',
     description='TCP/Websocket bridge with JSON message routing and reconnecting clients',
@@ -14,8 +28,7 @@ setup(
     ),
     include_package_data=True,
     long_description=open(path.join(_here, 'README.md')).read(),
-    install_requires=[
-    ],
+    install_requires=parse_requirements('subscriptionServer2/requirements.txt'),
     url='https://github.com/superLimitBreak/multisocketServer',
     author='Allan Callaghan',
     author_email='calaldees@gmail.com',
