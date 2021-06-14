92f3af41f0a8fdfd2e9aac7a815954362b2dfb90
Allan Callaghan <calaldees@gmail.com>
2021-06-14 23:28:34 +0100

    sort of got udp and websocket doing something

--- udp2ws/udp2ws.py
+++ udp2ws/udp2ws.py
@@ -1,4 +1,5 @@
 import asyncio
+import signal
 
 import websockets
 
@@ -23,7 +24,7 @@ class Udp2ws():
         log.info(f'onDisconnected: {source.remote_address}')
 
     async def new_connection(self, websocket, path):
-        self.connections[websocket]
+        self.connections.add(websocket)
         self.onConnected(websocket)
         try:
             async for data in websocket:
@@ -33,16 +34,30 @@ class Udp2ws():
         except Exception as ex:
             log.exception('Error with websocket connection')
         finally:
-            del self.connections[websocket]
+            self.connections.remove(websocket)
             self.onDisconnected(websocket)
 
     async def serve_websocket(self):
-        return websockets.serve(self.new_connection, "0.0.0.0", self.port_websocket)
+        log.info("Starting Websocket server")
+        loop = asyncio.get_event_loop()
+        self.stop = loop.create_future()
+        loop.add_signal_handler(signal.SIGTERM, self.stop.set_result, None)
+        async def server_with_stop(stop):
+            async with websockets.serve(self.new_connection, "0.0.0.0", self.port_websocket):
+                await stop
+        await server_with_stop(self.stop)
+        #try:
+        #    loop.run_until_complete(server_with_stop(self.stop))
+        #except KeyboardInterrupt as ex:
+        #    pass
+        #finally:
+        #    log.info('Shutdown')
 
     async def serve_udp(self):
         # https://docs.python.org/3/library/asyncio-protocol.html#asyncio-udp-echo-server-protocol
         log.info("Starting UDP server")
-        return asyncio.get_running_loop().create_datagram_endpoint(
+        loop = asyncio.get_running_loop()
+        await loop.create_datagram_endpoint(
             self.EchoServerProtocol, 
             local_addr=('0.0.0.0', self.port_udp),
         )
@@ -57,6 +72,11 @@ class Udp2ws():
             log.info('Send %r to %s' % (message, addr))
             self.transport.sendto(data, addr)
 
+    async def run(self):
+        task1 = asyncio.create_task(self.serve_udp())
+        task2 = asyncio.create_task(self.serve_websocket())
+        await task1
+        await task2
 
 def get_args():
     import argparse
@@ -78,5 +98,8 @@ if __name__ == "__main__":
     kwargs = get_args()
     logging.basicConfig(level=kwargs['log_level'])
 
-    udp2ws = Udp2ws()
-    asyncio.run(udp2ws.run(**kwargs))
+    udp2ws = Udp2ws(**kwargs)
+    asyncio.run(udp2ws.run())
+    #udp2ws.serve_websocket()
+
+    # echo "This is my data" > /dev/udp/127.0.0.1/12462
